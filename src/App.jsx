9a31452baa2644e26e93dbcf26413a58e5cf2b73
Dylen <dylenbelanger2004@gmail.com>
2023-09-04 21:40:43 -0400

    Fetch data for first 151 pokemon (Gen 1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-import './App.css'
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  // API data will be fetched only on mount.
+  useEffect(() => {
+    const fetchData = async () => {
+      // Only get data for first 151 pokemon (GEN 1)
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
+      );
+      const data = await response.json();
+      console.log(data);
+    };
+    fetchData();
+  }, []);
 }
 
-export default App
+export default App;
