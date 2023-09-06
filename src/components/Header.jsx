497f16963a352e00a19c63f9f83139242d00f79f
Dylen <dylenbelanger2004@gmail.com>
2023-09-05 21:56:43 -0400

    Add instructions to header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,10 @@ function Header({ score, bestScore }) {
   return (
     <header>
       <h1>Memory Card Game</h1>
+      <p>
+        Get points by clicking on an image but don&apos;t click on any more than
+        once!
+      </p>
       <Scoreboard score={score} bestScore={bestScore} />
     </header>
   );
